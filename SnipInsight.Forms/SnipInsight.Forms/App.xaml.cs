84b9ef63e53ebe2b7634a7f5a97fa4d86faedb2c
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-06-13 17:18:18 +0200

    Added StyleCop

--- SnipInsight.Forms/SnipInsight.Forms/App.xaml.cs
+++ SnipInsight.Forms/SnipInsight.Forms/App.xaml.cs
@@ -3,32 +3,33 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
 namespace SnipInsight.Forms
 {
     public partial class App : Application
-	{
+    {
         public static readonly string Title = "Snip Insights";
 
-		public App ()
-		{
-			InitializeComponent();
+        public App()
+        {
+            InitializeComponent();
 
             MainPage = new HomePage();
-		}
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
