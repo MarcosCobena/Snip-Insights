2baa986f9a1b57459ba44c5cedc90760d1837529
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-06-13 10:24:22 +0200

    Added UWP & Mac Forms projects

---
+++ SnipInsight.Forms.Mac/AppDelegate.cs
@@ -0,0 +1,31 @@
+﻿using AppKit;
+using CoreGraphics;
+using Foundation;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace SnipInsight.Forms.Mac
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : FormsApplicationDelegate
+    {
+        NSWindow _window;
+
+        public override NSWindow MainWindow => _window;
+
+        public AppDelegate()
+        {
+            var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+            var rect = new CGRect(200, 1000, 1024, 768);
+            _window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            base.DidFinishLaunching(notification);
+        }
+    }
+}
