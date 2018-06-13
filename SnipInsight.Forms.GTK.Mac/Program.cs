2513a7d1d7eb82d9fa5dd9f70a437c6afe236615
Marcos Cobeña Morián <marcoscm.digital@gmail.com>
2018-06-13 11:23:51 +0200

    Added GTK Forms project (Mac)

---
+++ SnipInsight.Forms.GTK.Mac/Program.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms.Platform.GTK;
+
+namespace SnipInsight.Forms.GTK.Mac
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            global::Xamarin.Forms.Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle(App.Title);
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
